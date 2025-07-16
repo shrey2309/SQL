c85b5cdc3311443bf08639381636ed6f5051d1f1
Shrey Dubey <65900931+shrey2309@users.noreply.github.com>
2025-07-16 22:00:21 +0530

    Update DML_Commands_(Insert).sql

--- DML_Commands_(Insert).sql
+++ DML_Commands_(Insert).sql
@@ -2,7 +2,7 @@ use puma;
 select * from users;
 
 -- Insert
--- user_id, user_name, mail_id, phone, DOB, city, marital_status, sex
+-- Order in which fields are to be entered (user_id, user_name, mail_id, phone, DOB, city, marital_status, sex)
 insert into users values
 ("1001","Shrey","s@gmail.com","9836524702","2000-07-3","Delhi","Single","Male");
 insert into users values
@@ -16,4 +16,6 @@ insert into users(user_id,user_name,mail_id,phone) values
 insert into users(user_id,user_name,mail_id,phone) values
 ("1006","Abhay","h@gmail.com","7536982410"),
 ("1007","Ritika","r@gmail.com","8546971230");
+
+-- drop databse puma
 
