9128556e65893ba471fddd9c830477dd6c2a1da1
Shrey Dubey <65900931+shrey2309@users.noreply.github.com>
2025-07-18 19:13:38 +0530

    Update Other_DML_Commands_(Updat,Delete,Truncate).sql

--- Other_DML_Commands_(Updat,Delete,Truncate).sql
+++ Other_DML_Commands_(Updat,Delete,Truncate).sql
@@ -1,16 +1,21 @@
 use puma;
 
+-- Update Command
+
+-- Update using a Primary Key
 update users set user_name = 'Mishra' where user_id = '1002';
 update users set user_name = 'Vaibhav' , mail_id = 'vaibhi@gmail.com' where user_id = '1003';
 
+-- Update using a Non Primary Key
 update users set user_name = 'Shobhit' where user_name = 'Abhay' and mail_id = 'h@gmail.com';
 select * from users;
 
+-- Delete Command
 delete from users where user_id = '1004';
 delete from users where user_name = 'Ayushi' and mail_id = 'b@gmail.com';
 select * from users;
 
--- Truncate
+-- Truncate Command
 truncate table users;
 -- Won't work as users table has a column which references as a foreign key in another table
 
