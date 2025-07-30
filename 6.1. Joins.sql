de92917b4c89c1db73686cdfbffda3f185ac2760
Shrey Dubey <65900931+shrey2309@users.noreply.github.com>
2025-07-30 22:05:37 +0530

    Add files via upload

---
+++ 6.1. Joins.sql
@@ -0,0 +1,47 @@
+use gfg;
+
+#1 Left Join
+select * from employees;
+select * from salaries;
+select * from employees left join salaries 
+	   on employees.EmployeeID = salaries.EmployeeID;
+        
+-- Selecting only some columns
+select employees.EmployeeID , employees.FirstName , employees.LastName , salaries.salary 
+       from employees left join salaries 
+       on employees.EmployeeID = salaries.EmployeeID;
+       
+       
+#2 Right Join
+select * from employees;
+select * from salaries;
+select * from employees right join salaries 
+	    on employees.EmployeeID = salaries.EmployeeID;
+
+-- Selecting only some columns
+select employees.EmployeeID , employees.FirstName , employees.LastName , salaries.salary 
+       from employees right join salaries 
+       on employees.EmployeeID = salaries.EmployeeID;
+       
+
+#3 Inner Join
+select * from employees;
+select * from department;
+select * from employees t1 inner join department t2
+       on t1.DepartmentID = t2.DepartmentID ;
+       
+-- Joining different columns from all tables(FirstName , LastName , DepartmentName , Salary)
+select t1.FirstName , t1.LastName , t2.DepartmentName , t3.salary 
+       from employees t1 inner join department t2
+       on t1.DepartmentID = t2.DepartmentID 
+	   inner join salaries t3
+       on t1.EmployeeID = t3.EmployeeID;
+       
+ 
+#4 Cross Join
+select * from employees;
+select * from department;
+select * from employees cross join department; 
+select count(*) from employees cross join department;  
+
+
